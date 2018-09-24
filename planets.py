a03568cb66f4cb6301603d432e2529930981ed09
zackrogman <43486445+zackrogman@users.noreply.github.com>
2018-09-24 08:35:52 -0700

    Update planets.py

--- planets.py
+++ planets.py
@@ -1,8 +1,8 @@
 def weight_on_planets():
    a = input("What dow you weigh on earth? ") #asks for input
    w = int(a) #casting the given weight as an int
-   M = "On Mars you would weigh" #casting an string
-   J = "On Jupiter you would weigh" #casting a string
+   M = "On Mars you would weigh" #making a string variable
+   J = "On Jupiter you would weigh" #making a string variable
    if w >= 0: #making sure inputed weight is valid
       print("\n" + M,w*.38,"pounds.\n" + J,w*2.34,"pounds.") #output with casted strings and the calculations
    else: #if inputed weight is not valid
